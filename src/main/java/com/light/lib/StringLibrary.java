e05cc19e7f72bab74a75ea6fc673aabba230a283
Ellie Michaels <gsm2137@columbia.edu>
2014-06-04 23:45:42 -0400

    Going into Alpha testing; version number updated to 1.0.0.

--- src/main/java/com/light/lib/StringLibrary.java
+++ src/main/java/com/light/lib/StringLibrary.java
@@ -3,5 +3,5 @@ package com.light.lib;
 public class StringLibrary {
 	public static final String MODID = "light";
 	public static final String name = "The Lord of Light Mod";
-	public static final String version = "0.7.3";
+	public static final String version = "1.0.0";
 }
